95699bd9de32cf85a62b45f5266f8a1ab2d37167
dePartymarty <70229439+dePartymarty@users.noreply.github.com>
2022-12-06 13:06:47 -0600

    pushing to github

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,7 +17,10 @@ const rootReducer = combineReducers({
     cart: cartReducer,
 
 });
-
+// REDUCER: In redux, the reducers are the pure functions that contain the logic and calculation that needed to be performed on the 
+// state. These functions accept the initial state of the state being used and the action type. It updates the 
+// state and responds with the new state. This updated state is sent back to the view components of the react to
+// make the necessary changes. Basically, In short, we can say that Reducer’s work is to return the updated state and to also describe how the state changes.
 
 const store = configureStore({
     reducer: rootReducer,
