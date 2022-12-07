f71490ffc2fcc6568d96d8f114986430005a550f
dePartymarty <70229439+dePartymarty@users.noreply.github.com>
2022-12-07 00:16:16 -0600

    package change

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
+  
 }
 
 app.get("/", (req, res) => {
